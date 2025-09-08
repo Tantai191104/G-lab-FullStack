b595e20265eb3f9f27cc3887629e56599cc357d7
Tantai191104 <tantai231204@gmail.com>
2025-09-08 23:37:24 +0700

    add

--- server/src/modules/keycaps/keycaps.module.ts
+++ server/src/modules/keycaps/keycaps.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { KeycapsController } from './keycaps.controller';
 import { KeycapsService } from './keycaps.service';
+import { Keycap, KeycapSchema } from './schemas/keycaps.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Keycap.name, schema: KeycapSchema }])],
   controllers: [KeycapsController],
   providers: [KeycapsService],
+  exports: [KeycapsService],
 })
 export class KeycapsModule {}
